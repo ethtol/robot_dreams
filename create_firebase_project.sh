3aac69125928483677ed9a92688c7fb713a487ca
Yurii Martyniuk <88658887+ethtol@users.noreply.github.com>
2024-10-23 14:20:11 +0300

    Create create_firebase_project.sh

---
+++ create_firebase_project.sh
@@ -0,0 +1,9 @@
+##!/bin/bash
+########################################################################
+## Create Firebase project, database, rule and etc using firebase cli ##
+########################################################################
+(echo [0x42]\n; echo "$fb_env"; sleep 10; echo "$fb_env"; sleep 10; echo "$fb_env"; sleep 30; echo \n; sleep 5; echo No; sleep 5; echo No; sleep 10; echo \n;) | firebase init hosting --token $firebase_token
+(echo y; sleep 10; echo y; sleep 10; echo \n; sleep 15; echo database.rules.json; sleep 10; echo n;) | firebase init database --token $firebase_token
+firebase apps:create WEB $fb_env --project $fb_env  --token $firebase_token
+firebase apps:sdkconfig --project $fb_env  --token $firebase_token > firebaseconfig.txt
+firebase deploy --token $firebase_token                   
